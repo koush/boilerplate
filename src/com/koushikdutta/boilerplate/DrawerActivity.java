22e91338fe890a55ec4a634b62723d69063603ca
Koushik Dutta <koushd@gmail.com>
2016-07-04 17:24:36 -0700

    fix

--- src/com/koushikdutta/boilerplate/DrawerActivity.java
+++ src/com/koushikdutta/boilerplate/DrawerActivity.java
@@ -30,7 +30,7 @@ public abstract class DrawerActivity extends WindowChromeCompatActivity {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.drawer_activity);
